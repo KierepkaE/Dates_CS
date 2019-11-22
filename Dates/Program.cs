27e041d7999b4c3cca392389a2a988ff1192b748
kierepkae <kierepkaedyta@gmail.com>
2019-11-22 14:57:01 +0100

    use TimeSpan

--- Dates/Program.cs
+++ Dates/Program.cs
@@ -14,6 +14,14 @@ namespace Dates
 
             var tomorrow = now.AddDays(1);
             var yesterday = now.AddDays(-1);
+
+            var timeSpan = new TimeSpan(1,2,3);
+            var timeSpan2 = TimeSpan.FromHours(1);
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(4);
+            var duration = end - start;
+            Console.WriteLine("Duration" + duration);
+
         }
 
         }
