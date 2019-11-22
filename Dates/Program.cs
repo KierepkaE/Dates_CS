cfbad25fcb4839eb575e54dd1121d799554c2839
kierepkae <kierepkaedyta@gmail.com>
2019-11-22 13:59:34 +0100

    use DateTime object with: Now, Today, AddDays, Hour, Minute

--- Dates/Program.cs
+++ Dates/Program.cs
@@ -6,7 +6,17 @@ namespace Dates
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Dates!");
+            var dateTime = new DateTime(2015, 1, 1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+            Console.WriteLine(now.Hour);
+            Console.WriteLine(now.Minute);
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+        }
+
         }
     }
-}
+    
+
